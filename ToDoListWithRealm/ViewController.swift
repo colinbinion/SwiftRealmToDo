30a9218036404fcd83f6d4a4d0ad5c9fea355d65
colinbinion <colin.binion@me.com>
2016-10-24 11:15:42 -0600

    Initial Commit

---
+++ ToDoListWithRealm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoListWithRealm
+//
+//  Created by Colin Binion on 10/24/16.
+//  Copyright © 2016 Colin Binion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
